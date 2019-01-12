42c2f555269761c168bb70f3281c841828db625d
Jack Noordhuis <daswaggamer@gmail.com>
2019-01-12 12:07:59 +1100

    Add clone script for local development

---
+++ clone.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# simple script to clone from git for local development
+
+git clone git@github.com:JackNoordhuis/DiscordRelay-PocketMine.git DiscordRelay
+git clone https://github.com/JackNoordhuis/DiscordRelay-PocketMine.wiki.git DiscordRelay/wiki
